240a7cdf3e38c996fa8efcee2d9b0e04fa071038
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-05-18 21:33:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,112 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
 
-# data importing
-data = pd.read_excel('Product recommendation.xls')
-df = pd.DataFrame(data=data)
-df.drop(columns=['Unnamed: 0'], inplace=True)
-array_df = pd.crosstab(df['Product'], df['EMAIL'])
-array_df = pd.DataFrame(array_df)
-
-# TfidfVectorizer
-vectorizer = TfidfVectorizer()
-feature_vectors = vectorizer.fit_transform(array_df)
-
-# Cosine Similarity
-similarity = cosine_similarity(feature_vectors)
-
-df = pd.DataFrame(similarity, index=array_df.index, columns=array_df.index)
+# data importing 
+data = pd.read_excel("Product Recommendation engine.xlsx")
+df = data.pivot_table(index="product", columns="email", values="rating").fillna(0)
+df_matrix = csr_matrix(df.values) # converting the table to an array matrix
+model_knn = NearestNeighbors(metric="cosine", algorithm="brute")
+model_knn.fit(df_matrix)
 
 
+def recom_system(distances, indices, index):
+    pro = None
+    rec = []
+    for i in range(0, len(distances.flatten())):
+        if i == 0:
+            pro = df.index[index].values[0]
+        else:
+            rec.append(df.index[indices.flatten()[i]])
+    return pro, rec
+
+
+
+
+def callback():
+    st.session_state.button_clicked = True
+    # st.session_state["2"] = True
+    # st.session_state["1"] = False
+def home():
+    st.session_state.button_clicked = False
 
 
 def main():
     st.title("Product Recommendation System")
     st.write("Banking Products")
-    st.write(df[['Product', 'EMAIL']].head())
-    st.write(" ")
-    mailid = st.text_input("Enter you EMAIL")
+    if st.checkbox("Registered Users", on_change=home):
+        st.write(data[["email","product","rating"]].head())
+    user = st.text_input("Enter your mail")
     if st.button("Enter"):
-        st.write(" ")
-        if mailid in list(df["EMAIL"].unique()):
-            st.write("Welcome back!")
-            df.set_index(['EMAIL'], inplace=True)
-            i = df[df.index == EMAIL]
-            last_product = len(i['Product'].values)
-            val = i['Product'].values[last_product - 1]
-            st.write("your current product:", val)
+        if user in list(data["email"].unique()):
+            st.write(f"Welcome back! {user[:9].upper()}")
+            index = data[data["email"]==user].index.values
+            # print(index)
+            distances, indices = model_knn.kneighbors(df.iloc[index,:].values.reshape(1,-1), n_neighbors=6)
+            product, rec = recom_system(distances, indices, index)
+            st.write(f"your current product : ' __{product}__ '")
             st.write("#### Products you may like ")
-            col1, col2, col3 = st.columns(3)
-            rec_pro = (df[val].sort_values(ascending=False))
-            list1 = []
-            for final_prouduct in rec_pro[1:4].index:
-                list1.append(final_prouduct)
+            col1, col2, col3, col4, col5  = st.columns(5)
             with col1:
-                st.success(list1[0])
-
+                st.write(f"__{rec[0]}__")
+                st.image("https://i.gifer.com/JVX7.gif")
             with col2:
-                st.success(list1[1])
-
+                st.write(f"__{rec[1]}__")
+                st.image("https://i.gifer.com/JVX7.gif")
             with col3:
-                st.success(list1[2])
+                st.write(f"__{rec[2]}__")
+                st.image("https://i.gifer.com/JVX7.gif")
+            with col4:
+                st.write(f"__{rec[3]}__")
+                st.image("https://i.gifer.com/JVX7.gif")
+            with col5:
+                st.write(f"__{rec[4]}__")
+                st.image("https://i.gifer.com/JVX7.gif")
+            st.button("home")
         else:
-            st.write("User not found!")
+            st.error("User not Registered")
+            st.write("create a new account or use registered email")
+    # st.markdown("##### Create a Account")
+    else:
+        if "button_clicked" not in st.session_state:
+            st.session_state.button_clicked = False
+        if (st.button("Create Account") or st.session_state.button_clicked):
 
-    st.write(" ")
-    st.write("Project by Akshay Narvate")
+            user_mail = st.text_input("Your Email")
+            # st.session_state
+            if (st.button("Sign up!", on_click=callback) or st.session_state.button_clicked):
+                st.success("Successful")
+                st.subheader("Welcome new user!")
+                st.write("Welcome to our Bank")
+                select = st.multiselect("Products Available", list(data["product"].unique()))
+                # st.write("your selected product: ",select)
+                st.write("#### Some popular products ")
+                rec2 = list(data.loc[data["rating"]>4.9, "product"].unique())[:6]
+                print(rec2)
+                col1, col2, col3, col4, col5  = st.columns(5)
+                with col1:
+                    st.write(f"__{rec2[0]}__")
+                    st.image("https://i.gifer.com/9uff.gif")
+                with col2:
+                    st.write(f"__{rec2[1]}__")
+                    st.image("https://i.gifer.com/9uff.gif")
+                with col3:
+                    st.write(f"__{rec2[2]}__")
+                    st.image("https://i.gifer.com/9uff.gif")
+                with col4:
+                    st.write(f"__{rec2[3]}__")
+                    st.image("https://i.gifer.com/9uff.gif")
+                with col5:
+                    st.write(f"__{rec2[4]}__")
+                    st.image("https://i.gifer.com/9uff.gif")
+                st.balloons()
+                st.button("home", on_click=home)
 
+
+    st.write("## Thank you for Visiting \nProject by Akshay Narvate")
+    st.markdown("<h1 style='text-align: right; color: #d7e3fc; font-size: small;'><a href='https://github.com/akshaynarvate/Product-Recommendation-engine'>Looking for Source Code?</a></h1>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
